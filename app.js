f5cfcd061ed23c34448fbb544bef85030d0285cb
Bilal Keskin <bilalkeskin@Bilal-MacBook-Pro.local>
2021-04-24 06:40:24 +0300

    project v1

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ let direction = 1;
 const width = 10;
 let appleIndex = 0;
 let score = 0;
+let intervalTime = 1000;
+let speed = 0.9;
+let timerId = 0;
 
 function createGrid() {
   //create 100 of these elements with a for loop
@@ -24,6 +27,20 @@ function createGrid() {
 createGrid();
 
 currentSnake.forEach((index) => squares[index].classList.add('snake'));
+
+function start() {
+  currentSnake.forEach((index) => squares[index].classList.remove('snake'));
+  squares[appleIndex].classList.remove('apple');
+  clearInterval(timerId);
+  currentSnake = [2, 1, 0];
+  score = 0;
+  scoreDisplay.textContent = score;
+  direction = 1;
+  intervalTime = 1000;
+  generateApple();
+  currentSnake.forEach((index) => squares[index].classList.add('snake'));
+  timerId = setInterval(move, intervalTime);
+}
 
 function move() {
   if (
@@ -49,23 +66,24 @@ function move() {
     squares[currentSnake[0]].classList.remove('apple');
     //grow our snake by adding class of snake to it
     squares[tail].classList.add('snake');
+    console.log(tail);
     //grow our snake array
     currentSnake.push(tail);
+    console.log(currentSnake);
     //generate new apple
     generateApple();
     //add one to the score
     score++;
     //display our score
     scoreDisplay.textContent = score;
-
     //speed up our snake
+    clearInterval(timerId);
+    intervalTime = intervalTime * speed;
+    timerId = setInterval(move, intervalTime);
   }
 
   squares[currentSnake[0]].classList.add('snake');
 }
-move();
-
-let timerId = setInterval(move, 1000);
 
 function generateApple() {
   do {
@@ -74,11 +92,6 @@ function generateApple() {
   squares[appleIndex].classList.add('apple');
 }
 generateApple();
-
-// 39 is right arrow
-// 38 is for the up arrow
-// 37 is for the left arrow
-// 40 is for the down arrow
 
 function control(e) {
   if (e.keyCode === 39) {
@@ -96,3 +109,4 @@ function control(e) {
   }
 }
 document.addEventListener('keyup', control);
+startButton.addEventListener('click', start);
